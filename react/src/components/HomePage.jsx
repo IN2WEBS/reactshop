50c17570e28bff66d7c01a14035033e43134973b
Rolandas <kazrolandas@gmail.com>
2018-06-02 10:05:41 +0300

    print out items with prices

--- react/src/components/HomePage.jsx
+++ react/src/components/HomePage.jsx
@@ -1,19 +1,35 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 
 class HomePage extends Component {
 
     render() {
+        console.log('this.props.shopItems', this.props.shopItems);
+        const allItems = this.props.shopItems && this.props.shopItems.map((item, i) => {
+            const price = item.price && item.price.amounts.USD;
+            console.log('price',price);
+                return (
+                    <div key={i}>
+                        <img src={item.image} alt=""/>
+                        <p>{price}</p>
+                    </div>
+                );
+            }
+        );
         return (
             <div className="homepage">
-                <Link to="/item">
-                    <button>Item</button>
-                </Link>
-                This is homepage
+                {allItems}
             </div>
         );
     }
 }
 
-export default HomePage;
+const mapStateToProps = (state) => {
+    return {
+        shopItems: state.items,
+    }
+};
+
+export default connect(mapStateToProps)(HomePage)
